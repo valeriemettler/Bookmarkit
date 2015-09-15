c4cae5ebba1a8974394a26ebfaafe3197b227416
Valerie Mettler <valeriemettler@gmail.com>
2015-09-15 00:28:19 -0700

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 var myDataRef = new Firebase('https://boiling-torch-464.firebaseio.com/');
   $('#tagInput').keypress(function (e) {
-    console.log(e.keyCode);
-    if (e.keyCode == 13) {
+    //console.log(e.keyCode);
+    if (e.keyCode == 13) {  //13 is the keycode for enter
       var url = $('#urlInput').val();
       var text = $('#tagInput').val();
       myDataRef.push({url: url, text: text});
-      //$('#urlInput').val('');
       $('#tagInput').val('');
       $('#urlInput').focus().val('');
     }
@@ -18,6 +17,7 @@ myDataRef.on('child_added', function(snapshot) {
 
 function displayBookmark(url, text) {
   var x = "";
-  x = x + '<div><a href="http://www.' + url + '" target="_blank">' + url + '</a> #' + text + '</div>';
+  x = x + '<div><a href="http://www.' + url
+  + '" target="_blank">' + url + '</a> #' + text + '</div>';
   $("#bookmarkDiv").append(x);
 };
