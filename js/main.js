df26c709163ea6088d03cdf1ef3651eebec58bf7
Valerie Mettler <valeriemettler@gmail.com>
2015-10-01 22:25:03 -0700

    retrieve tag without hash from url

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ var fb = new Firebase('https://boiling-torch-464.firebaseio.com/');
 var d = {};
 var item;
 
+var getTagUrl = function() {
+    $('#bookmarkDiv').on('click', '.taghash', function(event) {
+        event.stopPropagation();
+        var that = this;
+        var current_url_no_hash = that.hash.split('#')[1];
+       // var current_url_no_hash = window.location.hash.split('#')[1];
+        console.log(current_url_no_hash); //returns tag without hash
+    });
+};
+
 var display = function() {
     $('#input').keypress(function(e) {
         if (e.keyCode == 13) {
             var url = $('#urlInput').val();
             var text = $('#tagInput').val();
 
-            if (url === "" || text === ""){
+            if (url === "" || text === "") {
                 return;
             }
 
@@ -25,7 +35,7 @@ var display = function() {
 
             $('#tagInput').val('');
             $('#urlInput').focus().val('');
-      }
+        }
     });
 };
 
@@ -42,17 +52,16 @@ $(document).ready(function() {
 
         var x = "";
 
-         // var x = "";
+        // var x = "";
         for (item in d) {
-            x = x + '<div class="tag"><a href="#' + item + '">#' + item + '</a></div>';
+            x = x + '<div class="tag"><a href="#' + item + '" class="taghash">#' + item + '</a></div>';
 
             for (var i = 0; i < d[item].length; i++) {
-                x = x + '<div class="urls"><a href="http://www.' + d[item][i]
-                + '" target="_blank">' + d[item][i] + '</a></div>';
+                x = x + '<div class="urls"><a href="http://www.' + d[item][i] + '" target="_blank">' + d[item][i] + '</a></div>';
             }
         }
         $("#bookmarkDiv").html(x);
     })
-
+    getTagUrl();
     display();
 });
