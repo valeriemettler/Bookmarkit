09bd0f5a08df944ebe2b24d9896aaa0ccebeecf0
Valerie Mettler <valeriemettler@gmail.com>
2015-10-02 00:28:18 -0700

    updated view all bookmarks link to # and fixed function to display all with current tag undefined

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var fb = new Firebase('https://boiling-torch-464.firebaseio.com/');
 var d = {};
 var item;
 
+//
 
 function displayCurrentTag() {
     var current_tag = window.location.hash.split('#')[1];
@@ -9,16 +10,49 @@ function displayCurrentTag() {
 }
 window.onhashchange = displayCurrentTag;
 
+// var displayAll = function() {
+//     $('.all').on('click', function(event) {
+//         event.stopPropagation();
+//         console.log("hey!!");
+//          var x = "";
+
+//         for (item in d) {
+//             x = x + '<div class="tag"><a href="#' + item + '" class="taghash">#' + item + '</a></div>';
+
+//             for (var i = 0; i < d[item].length; i++) {
+//                 x = x + '<div class="urls"><a href="http://www.' + d[item][i] + '" target="_blank">' + d[item][i] + '</a></div>';
+//             }
+//         }
+//         $("#bookmarkDiv").html(x);
+
+//     });
+// };
+
 var displayTag = function(current_tag) {
-    var x = "";
+    console.log(current_tag);
+    if (current_tag === undefined) {
+        var x = "";
 
-    x = x + '<div class="tag">#' + current_tag + '</div>';
+        for (item in d) {
+            x = x + '<div class="tag"><a href="#' + item + '" class="taghash">#' + item + '</a></div>';
 
-    for (var i = 0; i < d[current_tag].length; i++) {
-        x = x + '<div class="urls"><a href="http://www.' + d[current_tag][i] + '" target="_blank">' + d[current_tag][i] + '</a></div>';
+            for (var i = 0; i < d[item].length; i++) {
+                x = x + '<div class="urls"><a href="http://www.' + d[item][i] + '" target="_blank">' + d[item][i] + '</a></div>';
+            }
+        }
+    } else {
+
+        var x = "";
+
+        x = x + '<div class="tag">#' + current_tag + '</div>';
+
+        for (var i = 0; i < d[current_tag].length; i++) {
+            x = x + '<div class="urls"><a href="http://www.' + d[current_tag][i] + '" target="_blank">' + d[current_tag][i] + '</a></div>';
+        }
     }
 
     $("#bookmarkDiv").html(x);
+
 };
 
 var display = function() {
@@ -70,5 +104,6 @@ $(document).ready(function() {
         }
         $("#bookmarkDiv").html(x);
     })
+    //displayAll();
     display();
 });
